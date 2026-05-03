7da7cfd5b0ee4ca5da63d958e5438e65c9938d75
ReggeMeister <mattiaswaern04@gmail.com>
2026-05-04 00:15:07 +0200

    bug fixes
    now it works, yippie

--- Fullstack/backend/routes/reviews.js
+++ Fullstack/backend/routes/reviews.js
@@ -5,11 +5,11 @@ const requireAuth = require ('../middleware/auth');
 
 router.post('/', requireAuth, (req, res) => {
     const { book_id, rating, text} = req.body;
-    if(!body_id || !rating || text)
+    if(!book_id || !rating || !text)
         return res.status(400).json({error: 'book_id, betyg och text krävs'});
 
     const result = db.prepare(
-        'INSERT INTO reviews (book_id, user_id, rating, text) VALUES (?, ?, ?, ?'
+        'INSERT INTO reviews (book_id, user_id, rating, text) VALUES (?, ?, ?, ?)'
      ).run(book_id, req.user.id, rating, text);
      res.status(201).json({id: result.lastInsertRowid, book_id, rating, text});
 });
